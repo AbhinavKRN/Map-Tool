ac086f096aadd23e5ac58844f3db4d85e21d70e3
AbhinavKRNarayan <abhinavv2005@gmail.com>
2024-10-20 14:51:04 +0530

    Responsive feature added

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMapEvents, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import PinForm from './PinForm';
 import fetchAddress from '../utils/fetchAddress';
@@ -7,55 +7,68 @@ import fetchAddress from '../utils/fetchAddress';
 import customPin from '../assests/custom-pin.png';
 
 const customIcon = new L.Icon({
-    iconUrl: customPin,
-    iconSize: [30, 40],
-    iconAnchor: [15, 40],
-    popupAnchor: [0, -40],
+  iconUrl: customPin,
+  iconSize: [30, 40],
+  iconAnchor: [15, 40],
+  popupAnchor: [0, -40],
 });
 
 const MapComponent = ({ addPin }) => {
-    const [position, setPosition] = useState(null);
+  const [position, setPosition] = useState(null);
+  const [formPosition, setFormPosition] = useState({ top: '20px', left: '20px' });
 
-    const handleMapClick = async (e) => {
+  const MapClickHandler = () => {
+    const map = useMap();
+
+    useMapEvents({
+      click: (e) => {
         const { lat, lng } = e.latlng;
         setPosition({ lat, lng });
-    };
 
-    const MapClickHandler = () => {
-        useMapEvents({
-            click: handleMapClick,
-        });
-        return null;
-    };
+        // Calculate the screen position of the clicked point
+        const point = map.latLngToContainerPoint([lat, lng]);
+        setFormPosition({ top: `${point.y}px`, left: `${point.x}px` });
+      },
+    });
 
-    const handleAddPin = async (remark) => {
-        if (!position) return;
-        const address = await fetchAddress(position.lat, position.lng);
-        addPin({ lat: position.lat, lng: position.lng, remark, address });
-        setPosition(null);
-    };
+    return null;
+  };
 
-    return (
-        <div style={{ position: 'relative' }}>
-            <MapContainer 
-                center={[51.505, -0.09]} 
-                zoom={13} 
-                style={{ height: '100vh', width: '100%' }}
-            >
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {position && <Marker position={position} icon={customIcon} />}
-                <MapClickHandler />
-            </MapContainer>
+  const handleAddPin = async (remark) => {
+    if (!position) return;
+    const address = await fetchAddress(position.lat, position.lng);
+    addPin({ lat: position.lat, lng: position.lng, remark, address });
+    setPosition(null);
+  };
 
-            {position && (
-                <div className="remark-form-container">
-                    <PinForm onSubmit={handleAddPin} />
-                </div>
-            )}
+  return (
+    <div style={{ position: 'relative' }}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        style={{ height: '100vh', width: '100%' }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {position && <Marker position={position} icon={customIcon} />}
+        <MapClickHandler />
+      </MapContainer>
+
+      {position && (
+        <div
+          className="remark-form-container"
+          style={{
+            position: 'absolute',
+            top: formPosition.top,
+            left: formPosition.left,
+            transform: 'translate(-50%, -100%)', 
+            zIndex: 1000,
+          }}
+        >
+          <PinForm onSubmit={handleAddPin} />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default MapComponent;
